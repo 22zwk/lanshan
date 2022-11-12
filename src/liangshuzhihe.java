8df38d25b59bf11cc193382b6d34f48fe301a676
13972 <1397220031@qq.com>
2022-11-12 15:09:24 +0800

    作业

---
+++ src/liangshuzhihe.java
@@ -0,0 +1,16 @@
+public class liangshuzhihe {
+
+        public int[] twoSum(int[] nums, int target) {
+            int len = nums.length;
+            for (int i = 0; i < len - 1; i++) {
+                for (int j = i + 1; j < len; j++) {
+                    if (nums[i] + nums[j] == target) {
+                        return new int[]{i, j};
+                    }
+                }
+            }
+
+            return nums;
+        }
+    }
+
